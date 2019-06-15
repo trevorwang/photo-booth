0f8d519f20c9170176928bec43b315999dde8a02
Trevor Wang <trevor.wang@qq.com>
2019-06-15 13:24:44 +0800

    refactor: use picasso to load picture to avoid OOM

--- app/src/main/java/com/example/photobooth/ui/ImagePreviewActivity.kt
+++ app/src/main/java/com/example/photobooth/ui/ImagePreviewActivity.kt
@@ -1,10 +1,9 @@
 package com.example.photobooth.ui
 
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.photobooth.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.image_preview_activity.*
 
 class ImagePreviewActivity : AppCompatActivity() {
@@ -12,11 +11,7 @@ class ImagePreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.image_preview_activity)
-
         val uri = intent.getStringExtra("image_uri")
-        contentResolver.openInputStream(Uri.parse(uri)).let {
-            val bitmap = BitmapFactory.decodeStream(it)
-            preview.setImageBitmap(bitmap)
-        }
+        Picasso.get().load(uri).into(preview)
     }
 }
